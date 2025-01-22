36fc92f3641812dc5f25498b8bc3ee19071bad63
maksimiliantim <112427437+maksimiliantim@users.noreply.github.com>
2025-01-22 21:14:19 +0300

    Update urls.py

--- exam_system/urls.py
+++ exam_system/urls.py
@@ -1,11 +1,10 @@
-# exam_system/urls.py
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
 urlpatterns = [
-    path('admin/', admin.site.urls),  # Админка
-    path('', lambda request: redirect('auth_view')),  # Перенаправление на auth/
-    path('', include('users_app.urls')),  # Общая страница входа и регистрации
-    path('tests/', include('tests_app.urls')),  # Тесты (пользовательская панель)
+    path('admin/', admin.site.urls),  # админка
+    path('', lambda request: redirect('auth_view')),  # перенаправление на auth/
+    path('', include('users_app.urls')),  # общая страница входа и регистрации
+    path('tests/', include('tests_app.urls')),  # тесты (пользовательская панель)
 ]
 

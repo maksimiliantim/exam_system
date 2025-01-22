d0e031228cbecb3641b6d7fc0032938fe6f4c6fc
maksimiliantim <112427437+maksimiliantim@users.noreply.github.com>
2025-01-22 17:06:35 +0300

    Update urls.py

--- users_app/urls.py
+++ users_app/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import views as auth_views
 from . import views  # Импортируем views для использования auth_view
 from django.contrib.auth.views import LogoutView
 from .views import auth_view, SignUpView
-
+from .views import LogoutView
 urlpatterns = [
     path('auth/', views.auth_view, name='auth_view'),  # Общая страница для входа и регистрации
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),

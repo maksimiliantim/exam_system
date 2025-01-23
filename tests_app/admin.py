4d2bb5c69c447b7504b67956ee12b9b90adc3f32
maksimiliantim <112427437+maksimiliantim@users.noreply.github.com>
2025-01-23 23:45:04 +0300

    Update admin.py

--- tests_app/admin.py
+++ tests_app/admin.py
@@ -1,8 +1,25 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
 from .models import Subject, Test, Question, Answer, TestResult
 
-admin.site.register(Subject)
-admin.site.register(Test)
-admin.site.register(Question)
-admin.site.register(Answer)
-admin.site.register(TestResult)
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Test)
+class TestAdmin(admin.ModelAdmin):
+    list_display = ('title', 'subject', 'access_key')
+
+@admin.register(Question)
+class QuestionAdmin(ImportExportModelAdmin):
+    list_display = ('text', 'test')
+    search_fields = ('text',)
+
+@admin.register(Answer)
+class AnswerAdmin(ImportExportModelAdmin):
+    list_display = ('text', 'question', 'is_correct')
+    search_fields = ('text',)
+
+@admin.register(TestResult)
+class TestResultAdmin(admin.ModelAdmin):
+    list_display = ('user', 'test', 'score', 'passed')

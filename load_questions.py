55dac82673cd37de28de0a2406a5a9b8323aef97
maksimiliantim <112427437+maksimiliantim@users.noreply.github.com>
2025-01-24 00:23:54 +0300

    Create load_questions.py

---
+++ load_questions.py
@@ -0,0 +1,18 @@
+import csv
+from tests_app.models import Question, Test
+
+test = Test.objects.get(title='Математика (1 семестр)')  
+with open('math_questions.csv', encoding='utf-8') as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        question = Question.objects.create(
+            text=row['Вопрос'],
+            test=test
+        )
+        answers = row['Ответы'].split(';')
+        correct_answer = row['Правильный ответ']
+        for answer_text in answers:
+            question.answers.create(
+                text=answer_text,
+                is_correct=(answer_text == correct_answer)
+            )

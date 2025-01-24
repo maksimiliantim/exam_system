28ba542735a2c124d61e614ede7f7eb1dc54e145
maksimiliantim <112427437+maksimiliantim@users.noreply.github.com>
2025-01-25 02:29:35 +0300

    Update admin.py

--- users_app/admin.py
+++ users_app/admin.py
@@ -1,1 +1,8 @@
 from django.contrib import admin
+from django.contrib import admin
+from import_export.admin import ExportMixin
+from .models import Question
+
+@admin.register(Question)
+class QuestionAdmin(ExportMixin, admin.ModelAdmin):
+    list_display = ('id', 'text', 'test')

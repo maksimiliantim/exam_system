e68261d4c6682e5787a6a7eca011d8d521dcd795
maksimiliantim <112427437+maksimiliantim@users.noreply.github.com>
2025-01-24 22:45:15 +0300

    Delete tests_app/tests_app directory

--- tests_app/tests_app/templatetags/custom_filters.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def dictkey(value, key):
-    """Фильтр для получения значения из словаря по ключу"""
-    return value.get(str(key), None)

08e1dff70dc4f975d4cf735864190fdc4a1c3b9d
maksimiliantim <112427437+maksimiliantim@users.noreply.github.com>
2025-01-25 00:23:16 +0300

    Update urls.py

--- tests_app/urls.py
+++ tests_app/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('test/<int:pk>/take/', views.take_test, name='take_test'),
     path('test/<int:pk>/result/', views.test_result, name='test_result'),
     path('test/<int:pk>/review/', views.test_review, name='test_review'),
+    path('auth/', auth_view, name='auth'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
 

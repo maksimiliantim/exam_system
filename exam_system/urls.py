b38dc7223cec517b681fa90ddb0bfcd65ab1c9eb
maksimiliantim <112427437+maksimiliantim@users.noreply.github.com>
2025-01-25 02:27:06 +0300

    Update urls.py

--- exam_system/urls.py
+++ exam_system/urls.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from import_export.admin import ExportMixin
 from django.shortcuts import redirect
-from .models import Test
+from tests_app.models import Test
+
 urlpatterns = [
     path('admin/', admin.site.urls),  # админка
     path('', lambda request: redirect('auth_view')),  # перенаправление на auth/

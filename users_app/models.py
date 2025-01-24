9e47053ab1b98568fe7bb71f048cb4ec90c0a19b
maksimiliantim <112427437+maksimiliantim@users.noreply.github.com>
2025-01-25 02:33:54 +0300

    Update models.py

--- users_app/models.py
+++ users_app/models.py
@@ -1,2 +1,5 @@
 # users_app/models.py
 from django.db import models
+class Question(models.Model):
+    test = models.ForeignKey('tests_app.Test', on_delete=models.CASCADE)
+    text = models.TextField()

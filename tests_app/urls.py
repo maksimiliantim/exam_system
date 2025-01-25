d1e8083aeb178082eaeebe1c1a11e897c3ff1cd7
maksimiliantim <112427437+maksimiliantim@users.noreply.github.com>
2025-01-25 05:53:50 +0300

    Update urls.py

--- tests_app/urls.py
+++ tests_app/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 urlpatterns = [
-    path('', views.TestListView.as_view(), name='test_list'),  # список всех тестов
-    path('dashboard/', views.user_dashboard, name='user_dashboard'),  # Панель пользователя
+    path('', views.TestListView.as_view(), name='test_list'),  
+    path('dashboard/', views.user_dashboard, name='user_dashboard'),  
     path('enter-key/', views.EnterKeyView.as_view(), name='enter_key'),
     path('test/<int:pk>/', views.TestDetailView.as_view(), name='test_detail'),
     path('test/<int:pk>/start/', views.start_test, name='start_test'),
